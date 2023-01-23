75b5767f479c0a9283b6f34e954bd767101ef145
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-01-23 18:45:52 +0600

    add navbar-app folder in app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,10 @@ const http = require("http");
 const { readFileSync } = require("fs");
 
 // get all files
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync('./navbar-app/index.html');
+const homePageStyle = readFileSync('./navbar-app/styles.css');
+const homePageBrowserJs = readFileSync('./navbar-app/browser-app.js');
+const homePageLogo = readFileSync('./navbar-app/logo.svg');
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
@@ -13,15 +16,39 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
     res.end();
-    //about page
-  } else if (url === "/about") {
+
+  } 
+      //about page
+    else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page us</h1>");
+    res.write("<h1>about page</h1>");
     res.end();
-    // if not match url
-  } else {
+
+  } 
+      // style.css
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homePageStyle);
+    res.end();
+
+  } 
+  //logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homePageLogo);
+    res.end();
+  }
+  //browser.js
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homePageBrowserJs);
+    res.end();
+  }
+
+      // if not match url
+  else {
     res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>page not found</h1>");
+    res.write("<h1>page not found are you looking for</h1>");
     res.end();
   }
 });
