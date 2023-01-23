9620dc8be6edc21d71cb08e9b6e67044bddb583f
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-01-23 21:50:18 +0600

    create new file nodemon.json for legecy watch

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,2 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
-app.get('/', (req, res) => {
-  res.json(products[0].name)
-})
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+console.log('hello people');
+console.log('hello world');
