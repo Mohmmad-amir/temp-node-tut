54fece3af0e10379b1969b531b1553791a9f702d
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-01-23 19:44:41 +0600

    practice express static function

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,55 +1,10 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require('express')
+const app = express()
+const { products } = require('./data')
+app.get('/', (req, res) => {
+  res.json(products[0].name)
+})
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html');
-const homePageStyle = readFileSync('./navbar-app/styles.css');
-const homePageBrowserJs = readFileSync('./navbar-app/browser-app.js');
-const homePageLogo = readFileSync('./navbar-app/logo.svg');
-
-const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  // console.log('user hit the server');
-  const url = req.url;
-  //home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-
-  } 
-      //about page
-    else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page</h1>");
-    res.end();
-
-  } 
-      // style.css
-  else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homePageStyle);
-    res.end();
-
-  } 
-  //logo
-  else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homePageLogo);
-    res.end();
-  }
-  //browser.js
-  else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homePageBrowserJs);
-    res.end();
-  }
-
-      // if not match url
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>page not found are you looking for</h1>");
-    res.end();
-  }
-});
-server.listen(5000);
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
